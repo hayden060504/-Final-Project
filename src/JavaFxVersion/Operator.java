bb19fed384d591a9b8e452024da8aefb664d6cb7
GDZ0mb1e <seantsai91@gmail.com>
2025-04-30 17:52:09 +0800

    建立Operator

--- src/JavaFxVersion/Operator.java
+++ src/JavaFxVersion/Operator.java
@@ -2,12 +2,12 @@ package JavaFxVersion;
 import javafx.application.*;
 import javafx.stage.Stage;
 
-public class QASettingPane extends Application {
+public class Operator extends Application{
 	
-	public void start(Stage stage) {
-		stage.show();
+	public void start(Stage primaryStage) {
+		primaryStage.show();
 	}
-	
+
 	public static void main(String args[]) {
 		launch(args);
 	}
