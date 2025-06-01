25052f41f2226fdab7aef2b02f99c206f260215f
Jammie-Liu <113306037@g.nccu.edu.tw>
2025-06-01 13:46:20 +0800

    update

--- src/Main/JavaFxFontTransfer.java
+++ src/Main/JavaFxFontTransfer.java
@@ -30,4 +30,11 @@ public class JavaFxFontTransfer {
 		return javafx.scene.paint.Color.rgb(awtColor.getRed(), awtColor.getGreen(), awtColor.getBlue(),
 				awtColor.getAlpha() / 255.0);
 	}
+	
+	 public static String toHex(javafx.scene.paint.Color color) {
+	        int r = (int) Math.round(color.getRed() * 255);
+	        int g = (int) Math.round(color.getGreen() * 255);
+	        int b = (int) Math.round(color.getBlue() * 255);
+	        return String.format("%02x%02x%02x", r, g, b);
+	 }
 }
